efcab9c0f21e303d355096c640e7b06f1da3087c
iGorTha <ylann.gerber2005@gmail.com>
2023-10-31 11:54:05 +0100

    [+] Add DB Connect

--- src/main/java/fr/igortha/shamaPass/Main.java
+++ src/main/java/fr/igortha/shamaPass/Main.java
@@ -1,10 +1,17 @@
 package fr.igortha.shamaPass;
 
 import fr.igortha.shamaPass.commands.XpCommand;
+import fr.igortha.shamaPass.db.DBConnect;
+import fr.igortha.shamaPass.placeholder.XpPassPlaceHolder;
+import fr.igortha.shamaPass.placeholder.XpPlaceHolder;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
+    @Getter
+    private DBConnect dbConnect;
 
     @Getter
     private static Main instance;
@@ -22,7 +29,26 @@ public class Main extends JavaPlugin {
             this.setEnabled(false);
             return;
         }
+
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") == null) {
+            this.getLogger().severe("Could not find PlaceholderAPI! This plugin is required.");
+            this.setEnabled(false);
+            return;
+        }
+        /////////////////////CONNECT_DB///////////////////////
+        this.dbConnect = new DBConnect();
+        dbConnect.mysqlSetup();
+        //////////////////REGISTER_COMMAND////////////////////
+
+        //////////////////REGISTER_COMMAND////////////////////
         this.loadCommand();
+        //////////////////////////////////////////////////////
+
+        ///////////////////PLACEHOLDER////////////////////////
+        new XpPassPlaceHolder().register();
+        new XpPlaceHolder().register();
+        //////////////////////////////////////////////////////
+
         this.getLogger().info("ShamaPass enabled!");
     }
 

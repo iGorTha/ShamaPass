73c67a85991eea340228c14674a5b94dc7a38bb5
iGorTha <ylann.gerber2005@gmail.com>
2024-03-02 11:15:26 +0100

    [~] Update System perm

--- src/main/java/fr/igortha/shamaPass/utils/Permissions.java
+++ src/main/java/fr/igortha/shamaPass/utils/Permissions.java
@@ -1,15 +1,20 @@
 package fr.igortha.shamaPass.utils;
 
 import fr.igortha.shamaPass.Main;
+import lombok.Getter;
 import org.bukkit.command.CommandSender;
 
+@Getter
 public enum Permissions {
     COMMAND_XP(Main.getInstance().getConfig().getString("permissions.command-xp")),
     ;
 
+    private final String perm;
+
     public static final String BASE_PERMISSION = Main.getInstance().getConfig().getString("permissions.base-permissions");
 
     Permissions(String perm) {
+        this.perm = perm;
     }
 
     public static boolean hasPermission(CommandSender sender, Permissions perm) {
